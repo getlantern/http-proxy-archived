1c3d8c9d78aee07f5fc5699d838c34ec09ab7493
Ulysses Aalto <uaalto@getlantern.org>
2015-09-29 17:06:13 +0200

    fffw code fully integrated

---
+++ log.go
@@ -0,0 +1,14 @@
+package main
+
+type timestamped struct {
+	w io.Writer
+}
+
+// timestamped adds a timestamp to the beginning of log lines
+func (t *timestamped) Write(buf []byte) (n int, err error) {
+	ts := time.Now()
+	runningSecs := ts.Sub(processStart).Seconds()
+	secs := int(math.Mod(runningSecs, 60))
+	mins := int(runningSecs / 60)
+	return fmt.Fprintf(t.w, "%s - %dm%ds %s", ts.In(time.UTC).Format(logTimestampFormat), mins, secs, buf)
+}
