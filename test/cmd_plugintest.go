c197ea74a7b16074bd60a0afddf0d6c8c2ac3fdc
Ulysses Aalto <uaalto@getlantern.org>
2015-09-29 12:45:38 +0200

    CONNECT and direct proxying for test command

--- test/cmd_plugintest.go
+++ test/cmd_plugintest.go
@@ -3,10 +3,45 @@ package main
 import (
 	//"../plugins"
 	"fmt"
-	"github.com/mailgun/oxy/forward"
+	"io"
+	"net"
 	"net/http"
 	"net/http/httputil"
+	"sync"
+
+	"github.com/mailgun/oxy/forward"
 )
+
+func respondOK(writer io.Writer, req *http.Request) error {
+	defer func() {
+		if err := req.Body.Close(); err != nil {
+			fmt.Printf("Error closing body of OK response: %s", err)
+		}
+	}()
+
+	resp := &http.Response{
+		StatusCode: http.StatusOK,
+		ProtoMajor: 1,
+		ProtoMinor: 1,
+	}
+
+	return resp.Write(writer)
+}
+
+func respondBadGateway(w io.Writer, msg string) {
+	fmt.Printf("Responding BadGateway: %v", msg)
+	resp := &http.Response{
+		StatusCode: http.StatusBadGateway,
+		ProtoMajor: 1,
+		ProtoMinor: 1,
+	}
+	err := resp.Write(w)
+	if err == nil {
+		if _, err = w.Write([]byte(msg)); err != nil {
+			fmt.Printf("Error writing error to io.Writer: %s", err)
+		}
+	}
+}
 
 func main() {
 	fwd, _ := forward.New()
@@ -18,7 +53,41 @@ func main() {
 			fmt.Println(err)
 		}
 		fmt.Println(string(dmp))
-		fwd.ServeHTTP(w, req)
+
+		if req.Method == "CONNECT" {
+			var clientConn net.Conn
+			var connOut net.Conn
+
+			respondOK(w, req)
+			if clientConn, _, err = w.(http.Hijacker).Hijack(); err != nil {
+				respondBadGateway(w, fmt.Sprintf("Unable to hijack connection: %s", err))
+				return
+			}
+			connOut, err = net.Dial("tcp", req.Host)
+			// Pipe data through CONNECT tunnel
+			closeConns := func() {
+				if clientConn != nil {
+					if err := clientConn.Close(); err != nil {
+						fmt.Printf("Error closing the out connection: %s", err)
+					}
+				}
+				if connOut != nil {
+					if err := connOut.Close(); err != nil {
+						fmt.Printf("Error closing the client connection: %s", err)
+					}
+				}
+			}
+			var closeOnce sync.Once
+			go func() {
+				_, _ = io.Copy(connOut, clientConn)
+				closeOnce.Do(closeConns)
+			}()
+			_, _ = io.Copy(clientConn, connOut)
+			closeOnce.Do(closeConns)
+			fmt.Println("== CONNECT DONE ==")
+		} else {
+			fwd.ServeHTTP(w, req)
+		}
 	})
 
 	s := &http.Server{
