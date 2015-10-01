d89cbfda5075b0b49fa92902e94598a37d26735d
Ulysses Aalto <uaalto@getlantern.org>
2015-10-01 14:34:32 +0200

    CONNECT testing

--- main_test.go
+++ main_test.go
@@ -1,13 +1,14 @@
 package main
 
 import (
-	"errors"
+	"flag"
 	"fmt"
 	"log"
 	"net"
 	"net/http"
 	"net/http/httptest"
 	"net/url"
+	"os"
 	"strconv"
 	"testing"
 	"time"
@@ -15,7 +16,133 @@ import (
 	"github.com/getlantern/testify/assert"
 )
 
-var servers []*httptest.Server
+const (
+	validToken = "mytoken"
+)
+
+var (
+	targetURL *url.URL
+	proxyAddr net.Addr
+)
+
+func TestMain(m *testing.M) {
+	flag.Parse()
+
+	// Set up mock target servers
+	defer stopMockServers()
+	site, _ := newMockServer("holla!")
+	targetURL, _ = url.Parse(site)
+	log.Printf("Started target site at %s\n", targetURL)
+
+	// Set up chained server
+	s, err := setUpNewServer()
+	if err != nil {
+		log.Println("Error starting proxy server")
+		os.Exit(1)
+	}
+	proxyAddr = s.listener.Addr()
+	log.Printf("Started proxy server at %s\n", proxyAddr.String())
+
+	os.Exit(m.Run())
+}
+
+func TestConnectNoToken(t *testing.T) {
+	connectReq := "CONNECT %s HTTP/1.1\r\nHost: %s\r\n\r\n"
+	connectResp := "HTTP/1.1 404 Not Found\r\n"
+
+	conn, err := net.Dial("tcp", proxyAddr.String())
+	if !assert.NoError(t, err, "should dial proxy server") {
+		t.FailNow()
+	}
+	defer func() {
+		assert.NoError(t, conn.Close(), "should close connection")
+	}()
+
+	req := fmt.Sprintf(connectReq, targetURL.Host, targetURL.Host)
+	t.Log("\n" + req)
+	_, err = conn.Write([]byte(req))
+	if !assert.NoError(t, err, "should write CONNECT request") {
+		t.FailNow()
+	}
+
+	var buf [400]byte
+	_, err = conn.Read(buf[:])
+	if !assert.Contains(t, string(buf[:]), connectResp,
+		"should get 404 Not Found because no token was provided") {
+		t.FailNow()
+	}
+}
+
+func TestConnectNoUID(t *testing.T) {
+	connectReq := "CONNECT %s HTTP/1.1\r\nHost: %s\r\nX-Lantern-Auth-Token: %s\r\n\r\n"
+	connectResp := "HTTP/1.1 404 Not Found\r\n"
+
+	conn, err := net.Dial("tcp", proxyAddr.String())
+	if !assert.NoError(t, err, "should dial proxy server") {
+		t.FailNow()
+	}
+	defer func() {
+		assert.NoError(t, conn.Close(), "should close connection")
+	}()
+
+	req := fmt.Sprintf(connectReq, targetURL.Host, targetURL.Host, validToken)
+	t.Log("\n" + req)
+	_, err = conn.Write([]byte(req))
+	if !assert.NoError(t, err, "should write CONNECT request") {
+		t.FailNow()
+	}
+
+	var buf [400]byte
+	_, err = conn.Read(buf[:])
+	if !assert.Contains(t, string(buf[:]), connectResp,
+		"should get 404 Not Found because no token was provided") {
+		t.FailNow()
+	}
+}
+
+func TestConnectOK(t *testing.T) {
+	connectReq := "CONNECT %s HTTP/1.1\r\nHost: %s\r\nX-Lantern-Auth-Token: %s\r\nX-Lantern-UID: %s\r\n\r\n"
+	connectResp := "HTTP/1.1 200 OK\r\n"
+
+	conn, err := net.Dial("tcp", proxyAddr.String())
+	if !assert.NoError(t, err, "should dial proxy server") {
+		t.FailNow()
+	}
+	defer func() {
+		assert.NoError(t, conn.Close(), "should close connection")
+	}()
+
+	req := fmt.Sprintf(connectReq, targetURL.Host, targetURL.Host, validToken, "1234-1234-1234-1243-1234-1234")
+	t.Log("\n" + req)
+	_, err = conn.Write([]byte(req))
+	if !assert.NoError(t, err, "should write CONNECT request") {
+		t.FailNow()
+	}
+
+	var buf [400]byte
+	_, err = conn.Read(buf[:])
+	if !assert.Contains(t, string(buf[:]), connectResp,
+		"should get 200 OK") {
+		t.FailNow()
+	}
+}
+
+func setUpNewServer() (*Server, error) {
+	s := NewServer(validToken)
+	var err error
+	ready := make(chan bool)
+	go func(err *error) {
+		if *err = s.ServeHTTP("localhost:0", &ready); err != nil {
+			fmt.Println("Unable to serve: %s", err)
+		}
+	}(&err)
+	<-ready
+	return s, err
+}
+
+// Mock servers are useful for emulating locally a target site for testing tunnels
+
+var mockServers []*httptest.Server
 
 type mockHandler struct {
 	writer func(w http.ResponseWriter)
@@ -58,143 +185,12 @@ func newMockServer(msg string) (string, *mockHandler) {
 	m := mockHandler{nil}
 	m.Msg(msg)
 	s := httptest.NewServer(&m)
-	servers = append(servers, s)
+	mockServers = append(mockServers, s)
 	return s.URL, &m
 }
 
 func stopMockServers() {
-	for _, s := range servers {
+	for _, s := range mockServers {
 		s.Close()
 	}
-}
-
-func TestCONNECT(t *testing.T) {
-	connectReq := "CONNECT %s HTTP/1.1\r\nHost: %s\r\n\r\n"
-	tunneledReq := "GET / HTTP/1.1\r\n\r\n"
-	connectResp := "HTTP/1.1 200 OK\r\n"
-	tunneledResp := "holla!\n"
-	defer stopMockServers()
-	site, _ := newMockServer("holla!")
-	u, _ := url.Parse(site)
-	log.Printf("Started target site at %s\n", u)
-
-	addr, _ := startServer(t)
-	log.Printf("Started proxy server at %s\n", addr)
-
-	con, err := net.Dial("tcp", addr.String())
-	if !assert.NoError(t, err, "should dial proxy server") {
-		return
-	}
-	defer func() {
-		assert.NoError(t, con.Close(), "should close connection")
-	}()
-
-	req := fmt.Sprintf(connectReq, u.Host, u.Host)
-	_, err = con.Write([]byte(req))
-	if !assert.NoError(t, err, "should write CONNECT request") {
-		return
-	}
-
-	var buf [400]byte
-	_, err = con.Read(buf[:])
-	if !assert.Contains(t, string(buf[:]), connectResp, "should get 200 OK for CONNECT") {
-		return
-	}
-
-	_, err = con.Write([]byte(tunneledReq))
-	if !assert.NoError(t, err, "should write tunneled data") {
-		return
-	}
-
-	_, err = con.Read(buf[:])
-	assert.Contains(t, string(buf[:]), tunneledResp, "should read tunneled response")
-}
-
-func TestGET(t *testing.T) {
-	log.Println("TestGET")
-	reqTemplate := "GET %s HTTP/1.1\r\nHost: %s\r\n\r\n"
-	resp := "HTTP/1.1 200 OK\r\n"
-	content := "holla!\n"
-	defer stopMockServers()
-	site, _ := newMockServer(content)
-	u, _ := url.Parse(site)
-	log.Printf("Started target site at %s\n", u)
-
-	addr, _ := startServer(t)
-	log.Printf("Started proxy server at %s\n", addr)
-
-	con, err := net.Dial("tcp", addr.String())
-	if !assert.NoError(t, err, "should dial proxy server") {
-		return
-	}
-	defer func() {
-		assert.NoError(t, con.Close(), "should close connection")
-	}()
-
-	req := fmt.Sprintf(reqTemplate, u, u.Host)
-	_, err = con.Write([]byte(req))
-	if !assert.NoError(t, err, "should write request") {
-		return
-	}
-
-	var buf [400]byte
-	_, err = con.Read(buf[:])
-	if !assert.Contains(t, string(buf[:]), resp, "should get 200 OK for GET") {
-		return
-	}
-	assert.Contains(t, string(buf[:]), content, "should read content")
-}
-
-func TestAuth(t *testing.T) {
-	log.Println("TestGET")
-	reqTemplate := "GET %s HTTP/1.1\r\nHost: %s\r\n\r\n"
-	resp := "HTTP/1.1 404 Not Found\r\n"
-	content := "holla!\n"
-	defer stopMockServers()
-	site, _ := newMockServer(content)
-	u, _ := url.Parse(site)
-	log.Printf("Started target site at %s\n", u)
-
-	addr, s := startServer(t)
-	s.Checker = func(req *http.Request) error {
-		return errors.New("dsafda")
-	}
-	log.Printf("Started proxy server at %s\n", addr)
-
-	con, err := net.Dial("tcp", addr.String())
-	if !assert.NoError(t, err, "should dial proxy server") {
-		return
-	}
-	defer func() {
-		assert.NoError(t, con.Close(), "should close connection")
-	}()
-
-	req := fmt.Sprintf(reqTemplate, u, u.Host)
-	_, err = con.Write([]byte(req))
-	if !assert.NoError(t, err, "should write request") {
-		return
-	}
-
-	var buf [400]byte
-	_, err = con.Read(buf[:])
-	assert.Contains(t, string(buf[:]), resp, "should get 404 Not Found")
-}
-
-func startServer(t *testing.T) (addr net.Addr, s *Server) {
-	l, err := net.Listen("tcp", "localhost:0")
-	if err != nil {
-		t.Fatalf("Unable to listen: %s", err)
-	}
-
-	s = &Server{
-		Dial: net.Dial,
-	}
-	go func() {
-		err := s.Serve(l)
-		if err != nil {
-			t.Fatalf("Unable to serve: %s", err)
-		}
-	}()
-
-	return l.Addr(), s
 }
