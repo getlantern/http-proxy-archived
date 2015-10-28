1a87fc994645becb0db5ff209a980ba7d58853c3
fffw <fffw@getlantern.org>
2015-10-28 10:05:18 +0800

    remove headers in Connection header

--- forward/utils.go
+++ forward/utils.go
@@ -17,10 +17,14 @@ func cloneURL(i *url.URL) *url.URL {
 // copyHeaders copies http headers from source to destination.  It does not
 // overide, but adds multiple headers
 func copyHeaders(dst, src http.Header) {
+	var extraHopByHopHeaders []string
 	for k, vv := range src {
 		switch k {
 		// Skip hop-by-hop headers, ref section 13.5.1 of http://www.ietf.org/rfc/rfc2616.txt
 		case "Connection":
+			// section 14.10 of rfc2616
+			// the slice is short typically, don't bother sort it to speed up lookup
+			extraHopByHopHeaders = vv
 			continue
 		case "Keep-Alive":
 			continue
@@ -37,8 +41,10 @@ func copyHeaders(dst, src http.Header) {
 		case "Upgrade":
 			continue
 		default:
-			for _, v := range vv {
-				dst.Add(k, v)
+			if !contains(k, extraHopByHopHeaders) {
+				for _, v := range vv {
+					dst.Add(k, v)
+				}
 			}
 		}
 	}
@@ -49,4 +55,13 @@ func removeHeaders(headers http.Header, names ...string) {
 	for _, h := range names {
 		headers.Del(h)
 	}
+}
+
+func contains(k string, s []string) bool {
+	for _, h := range s {
+		if k == h {
+			return true
+		}
+	}
+	return false
 }
