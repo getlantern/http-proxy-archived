b49455f6768dc30854182140c5a3b85b2b5e7cd6
Ulysses Aalto <uaalto@getlantern.org>
2015-11-04 11:40:26 -0500

    Fix #49.

--- src/github.com/getlantern/http-proxy/forward/forward.go
+++ src/github.com/getlantern/http-proxy/forward/forward.go
@@ -164,7 +164,7 @@ func (f *Forwarder) cloneRequest(req *http.Request, u *url.URL) (*http.Request, 
 	outReq.URL = cloneURL(req.URL)
 	outReq.URL.Scheme = scheme
 	outReq.URL.Host = req.Host
-	outReq.URL.Opaque = req.RequestURI
+	outReq.URL.Opaque = req.URL.Path
 	// raw query is already included in RequestURI, so ignore it to avoid dupes
 	outReq.URL.RawQuery = ""
 
