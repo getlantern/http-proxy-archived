4a0242bd8a58c020574e5cea522949fab6cbec21
Ulysses Aalto <uaalto@getlantern.org>
2015-11-11 13:34:02 -0500

    Moved tests to Lantern proxy

--- test/run-server.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env sh
-PROXY_DIR="src/github.com/getlantern/http-proxy"
-go run $PROXY_DIR/http_proxy.go $PROXY_DIR/server.go $PROXY_DIR/tls.go $PROXY_DIR/limitedlistener.go -addr=":8080" -token=111 $@
