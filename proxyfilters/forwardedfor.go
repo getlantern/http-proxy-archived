05d0220b4e0ba46ec51662ce2308ea84a971d2da
Ox Cart <ox.to.a.cart@gmail.com>
2017-07-09 21:52:06 -0500

    Only added forwarded for for non-CONNECT requests

--- proxyfilters/forwardedfor.go
+++ proxyfilters/forwardedfor.go
@@ -16,11 +16,13 @@ const (
 // AddForwardedFor adds an X-Forwarded-For header based on the request's
 // RemoteAddr.
 var AddForwardedFor = filters.FilterFunc(func(ctx context.Context, req *http.Request, next filters.Next) (*http.Response, error) {
-	if clientIP, _, err := net.SplitHostPort(req.RemoteAddr); err == nil {
-		if prior, ok := req.Header[xForwardedFor]; ok {
-			clientIP = strings.Join(prior, ", ") + ", " + clientIP
+	if req.Method != http.MethodConnect {
+		if clientIP, _, err := net.SplitHostPort(req.RemoteAddr); err == nil {
+			if prior, ok := req.Header[xForwardedFor]; ok {
+				clientIP = strings.Join(prior, ", ") + ", " + clientIP
+			}
+			req.Header.Set(xForwardedFor, clientIP)
 		}
-		req.Header.Set(xForwardedFor, clientIP)
 	}
 	return next(ctx, req)
 })
