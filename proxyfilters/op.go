d5e3084522d349d1466886ce2bdef5b21def592b
Ben Cartwright-Cox <ben@benjojo.co.uk>
2021-05-06 13:59:56 +0100

    Remove DNS errors from the error logs
    We don't need to know about these errors in our logging pipeline, given
    none of them are under our control.

--- proxyfilters/op.go
+++ proxyfilters/op.go
@@ -1,6 +1,8 @@
 package proxyfilters
 
 import (
+	e2 "errors"
+	"net"
 	"net/http"
 
 	"github.com/getlantern/errors"
@@ -30,7 +32,13 @@ var RecordOp = filters.FilterFunc(func(ctx filters.Context, req *http.Request, n
 		// Dumping stack trace is useful but in this case, it would create tons
 		// of noises as the filters are called recursively.
 		if e, ok := err.(errors.Error); ok {
-			log.Error(e.RootCause())
+			// Before we do this, we should check if this is a DNS error, if so we don't
+			// want to log it, since these log.Error's get sent up to stack driver and become
+			// super noisy!
+			var dnsError *net.DNSError
+			if !e2.As(e.RootCause(), &dnsError) {
+				log.Error(e.RootCause())
+			}
 		} else {
 			log.Error(err)
 		}
