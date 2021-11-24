6acae6ac862399dc4dc5919b866bf626ac59f2b1
Harry Harpham <harry@getlantern.org>
2021-11-23 18:04:41 -0700

    Suppress noisy network timeout errors

--- proxyfilters/op.go
+++ proxyfilters/op.go
@@ -1,7 +1,7 @@
 package proxyfilters
 
 import (
-	e2 "errors"
+	stderrors "errors"
 	"net"
 	"net/http"
 
@@ -20,20 +20,30 @@ var RecordOp = filters.FilterFunc(func(cs *filters.ConnectionState, req *http.Re
 	resp, nextCtx, err := next(cs, req)
 	if err != nil {
 		op.FailIf(err)
-		// Dumping stack trace is useful but in this case, it would create tons
-		// of noises as the filters are called recursively.
-		if e, ok := err.(errors.Error); ok {
-			// Before we do this, we should check if this is a DNS error, if so we don't
-			// want to log it, since these log.Error's get sent up to stack driver and become
-			// super noisy!
-			var dnsError *net.DNSError
-			if !e2.As(e.RootCause(), &dnsError) {
-				log.Error(e.RootCause())
-			}
-		} else {
-			log.Error(err)
-		}
+		logFilterError(err)
 	}
 	op.End()
 	return resp, nextCtx, err
 })
+
+func logFilterError(err error) {
+	var (
+		opErr  *net.OpError
+		dnsErr *net.DNSError
+	)
+	switch {
+	case stderrors.As(err, &opErr) && opErr.Timeout():
+		// Network timeouts are out of our control and create noise in StackDriver.
+		log.Debugf("%s timeout error: %v", opErr.Op, err)
+		return
+	case stderrors.As(err, &dnsErr):
+		// DNS errors are out of our control and create noise in StackDriver.
+		log.Debugf("DNS error: %v", err)
+		return
+	}
+	if e, ok := err.(errors.Error); ok {
+		// Filters are called recursively. We log only the root to reduce stack trace noise.
+		err = e.RootCause()
+	}
+	log.Error(err)
+}
